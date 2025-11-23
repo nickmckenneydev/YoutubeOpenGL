9c3525341f70c7473dd898de01213d5c4e70fe53
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-23 16:17:00 -0500

    Set attributes of SDL GL

--- main.cpp
+++ main.cpp
@@ -4,25 +4,43 @@
 #include <vector>
 #include <iostream>
 //GLOBALS
-SDL_Window* mWindow;
-SDL_Renderer* mRenderer;
+SDL_Window *mWindow=nullptr;
+SDL_Renderer *mRenderer=nullptr;
+SDL_GLContext gOpenGLContext = nullptr;
+bool gQuit = false;
 void InitalizeProgram(const char* title,int width,int height) {
 	if (SDL_Init(SDL_INIT_VIDEO))
 	{
 		std::cout << "SDL3 could not be loaded" << std::endl;
 		exit(1);
 	}
+		//OpenGL Version 3
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 1);
+
 		mWindow = SDL_CreateWindow(title, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 		mRenderer = SDL_CreateRenderer(mWindow, "opengl");
 		SDL_SetWindowTitle(mWindow, title);
-}
-void MainLoop() {
+		gOpenGLContext = SDL_GL_CreateContext(mWindow);
+		if (gOpenGLContext == nullptr)
+		{
+			std::cout << "OpenGL could not be loaded" << std::endl;
+		}
+}		
 
+void MainLoop() {
+	while (!gQuit) 
+	{
+
+	}
 }
 void CleanUp(){
+	
 	SDL_Quit();
 }
-int main(){}
+int main(){
+	InitalizeProgram();
+}
