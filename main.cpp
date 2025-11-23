51145994a02af5981bd4d5661e983fe75543bf4e
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-23 15:25:11 -0500

    project setup

--- main.cpp
+++ main.cpp
@@ -1,67 +1,38 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <SDL3/SDL.h>
+#include <string>
+#include <vector>
+#include <iostream>
+void InitalizeProgram() {
 
-int main()
-{
-	// Initialize GLFW
-	glfwInit();
-
-
-
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	// Introduce the window into the current context
-	glfwMakeContextCurrent(window);
-
-
-
-	//Load GLAD so it configures OpenGL
-	gladLoadGL();
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
-
-
-
-	// Specify the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// Clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swap the back buffer with the front buffer
-	glfwSwapBuffers(window);
-
-
-
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Take care of all GLFW events
-		glfwPollEvents();
-	}
-
-
-
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
-	return 0;
 }
+void MainLoop() {
+
+}
+void CleanUp(){}
+int main(){}
+//struct SDLApplication {
+//	SDL_Window* mWindow;
+//	SDL_Renderer* mRenderer;
+//	bool gameIsRunning;
+//	SDLApplication(const char* title, int width, int height) {
+//		SDL_Init(SDL_INIT_VIDEO);
+//		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+//		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+//		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+//		mWindow = SDL_CreateWindow(title, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+//		mRenderer = SDL_CreateRenderer(mWindow, "opengl");
+//		SDL_SetWindowTitle(mWindow, title);
+//	}
+//	~SDLApplication() {
+//		SDL_Quit();
+//	}
+//
+//};
+//
+//
+//int main(int argc, char* argv[])
+//{
+//	SDLApplication app("NICK NICK", 800, 600);
+//	return 0;
+//}
