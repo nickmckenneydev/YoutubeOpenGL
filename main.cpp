535f1200055ee098fcda3399ec46810cd82e64fd
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-23 15:31:51 -0500

    Attempting to create window

--- main.cpp
+++ main.cpp
@@ -3,36 +3,26 @@
 #include <string>
 #include <vector>
 #include <iostream>
-void InitalizeProgram() {
-
+//GLOBALS
+SDL_Window* mWindow;
+SDL_Renderer* mRenderer;
+void InitalizeProgram(const char* title,int width,int height) {
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cout << "SDL3 could not be loaded" << std::endl;
+		exit(1);
+	}
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+		mWindow = SDL_CreateWindow(title, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+		mRenderer = SDL_CreateRenderer(mWindow, "opengl");
+		SDL_SetWindowTitle(mWindow, title);
 }
 void MainLoop() {
 
 }
-void CleanUp(){}
+void CleanUp(){
+	SDL_Quit();
+}
 int main(){}
-//struct SDLApplication {
-//	SDL_Window* mWindow;
-//	SDL_Renderer* mRenderer;
-//	bool gameIsRunning;
-//	SDLApplication(const char* title, int width, int height) {
-//		SDL_Init(SDL_INIT_VIDEO);
-//		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-//		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-//		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-//		mWindow = SDL_CreateWindow(title, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-//		mRenderer = SDL_CreateRenderer(mWindow, "opengl");
-//		SDL_SetWindowTitle(mWindow, title);
-//	}
-//	~SDLApplication() {
-//		SDL_Quit();
-//	}
-//
-//};
-//
-//
-//int main(int argc, char* argv[])
-//{
-//	SDLApplication app("NICK NICK", 800, 600);
-//	return 0;
-//}
