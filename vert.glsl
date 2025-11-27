f87e000911f57dce5292adb4ad5ad3c9a2d06f76
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-27 14:01:25 -0500

    Following tutorial

--- vert.glsl
+++
@@ -1,7 +0,0 @@
-#version 410 core
-layout(location = 0) in vec3 position; // Note: vec3 to match your C++ float array
-
-void main()
-{
-    gl_Position = vec4(position.x, position.y, position.z, 1.0);
-}
