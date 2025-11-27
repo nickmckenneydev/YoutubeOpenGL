b64e28d71601984fffdd0d4d28467b25051e7709
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-27 14:07:48 -0500

    copied code

--- shaderClass.cpp
+++ shaderClass.cpp
@@ -1,5 +1,6 @@
-#include "shaderClass.h"
+#include"shaderClass.h"
 
+// Reads a text file and outputs a string with everything in the text file
 std::string get_file_contents(const char* filename)
 {
 	std::ifstream in(filename, std::ios::binary);
@@ -16,11 +17,14 @@ std::string get_file_contents(const char* filename)
 	throw(errno);
 }
 
+// Constructor that build the Shader Program from 2 different shaders
 Shader::Shader(const char* vertexFile, const char* fragmentFile)
 {
+	// Read vertexFile and fragmentFile and store the strings
 	std::string vertexCode = get_file_contents(vertexFile);
 	std::string fragmentCode = get_file_contents(fragmentFile);
 
+	// Convert the shader source strings into character arrays
 	const char* vertexSource = vertexCode.c_str();
 	const char* fragmentSource = fragmentCode.c_str();
 
@@ -39,7 +43,7 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile)
 	glCompileShader(fragmentShader);
 
 	// Create Shader Program Object and get its reference
-	GLuint ID = glCreateProgram();
+	ID = glCreateProgram();
 	// Attach the Vertex and Fragment Shaders to the Shader Program
 	glAttachShader(ID, vertexShader);
 	glAttachShader(ID, fragmentShader);
@@ -49,12 +53,17 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile)
 	// Delete the now useless Vertex and Fragment Shader objects
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+
 }
 
-void Shader::Activate() {
+// Activates the Shader Program
+void Shader::Activate()
+{
 	glUseProgram(ID);
 }
-void Shader::Delete() {
-	glDeleteProgram(ID);
 
+// Deletes the Shader Program
+void Shader::Delete()
+{
+	glDeleteProgram(ID);
 }
